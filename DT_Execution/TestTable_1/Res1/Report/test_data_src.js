ba15a37d068d4fc7072bcb606b50c6859ebe0e0a
Michael Gustus <michael.gustus@hpe.com>
2017-06-01 10:28:50 +0300

    data table execution

---
+++ DT_Execution/TestTable_1/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'c:\\Users\\gustus\\Documents\\Documentation\\Octane\\Programming\\UFT ALM integration\\UFT tests\\Data Table\\TestTable_1\\Res1',namePairs:[]}]};
