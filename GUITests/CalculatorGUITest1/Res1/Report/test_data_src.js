ef4fca80af6ef5178a0e1a032ce9fb3a07586442
Radislav Berkovich <radislav.berkovich@hpe.com>
2017-06-05 09:58:59 +0300

    fdf

--- GUITests/CalculatorGUITest1/Res1/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\bespalov\\UftTests\\GUITests\\CalculatorGUITest\\Res1',namePairs:[]}]};
--- GUITests/CalculatorGUITest2/Res2/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\bespalov\\UftTests\\GUITests\\CalculatorGUITest2\\Res2',namePairs:[]}]};
