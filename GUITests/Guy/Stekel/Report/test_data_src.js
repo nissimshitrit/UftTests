d78f6fe4e17b3fe06fb1c05bfae80860d561cf17
Radislav Berkovich <radislav.berkovich@microfocus.com>
2020-09-13 15:10:40 +0300

    remove

--- GUITests/Guy/Stekel/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Jenkins\\workspace\\UFT test run job - Suite ID 1064\\..\\..\\_test_sources\\https___github.com_radislavB_UftTests.git\\GUITests\\Guy\\Stekel',namePairs:[]}]};
